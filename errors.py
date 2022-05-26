c1094b3abae5b3eade4e8d91cf0a322396cf54e1
Wu Xin <wuhsin@zju.edu.cn>
2022-05-26 23:14:14 +0800

    add error classes

---
+++ errors.py
@@ -0,0 +1,13 @@
+class LoginError(Exception):
+    """Login Exception"""
+    pass
+
+
+class RegexMatchError(Exception):
+    """Regex Matching Exception"""
+    pass
+
+
+class DecodeError(Exception):
+    """JSON Decode Exception"""
+    pass
