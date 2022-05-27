d529617c5a8a478493abc7748db4581e68360585
Wu Xin <wuhsin@zju.edu.cn>
2022-05-27 23:53:20 +0800

    add dingtalk robot

---
+++ message.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+
+def dingtalk_robot(msg, access_token):
+    headers = {'Content-Type': 'application/json'}
+    webhook = 'https://oapi.dingtalk.com/robot/send?access_token=' + access_token
+    message = '[浙大健康打卡] ' + msg
+    payload = {
+        'msgtype': 'text', 
+        'text': {
+            'content': message
+        }
+    }
+    requests.post(webhook, data=json.dumps(payload), headers=headers)
+    print('发送成功', message)
+
+
+if __name__ == '__main__':
+    dingtalk_robot('test', 'xxx')
